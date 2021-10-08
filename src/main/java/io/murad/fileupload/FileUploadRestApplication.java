3ff249979ab39295b8bec78e563a259a1039c01d
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-10-08 23:25:38 +0600

    project created

---
+++ src/main/java/io/murad/fileupload/FileUploadRestApplication.java
@@ -0,0 +1,13 @@
+package io.murad.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/murad/fileupload/FileUploadRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.murad.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
