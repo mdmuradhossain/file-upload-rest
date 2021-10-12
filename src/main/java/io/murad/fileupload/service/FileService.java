af750efc86467faf660625442187645ba0ab1a98
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-10-12 14:09:48 +0600

    Filesevice interface added

---
+++ src/main/java/io/murad/fileupload/service/FileService.java
@@ -0,0 +1,18 @@
+package io.murad.fileupload.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface FileService {
+
+    public void init();
+
+    public void save(MultipartFile file);
+
+    public Resource load(String fileName);
+
+    public Stream<Path> loadAll();
+}
